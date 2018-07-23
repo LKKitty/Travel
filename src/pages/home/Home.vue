77cf14d279478d70052419ae6d94bfe48ded9e6e
LKKitty <LKKitty@git.com>
2018-07-23 16:56:36 +0800

    projuect init

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>hello world</div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+    }
+  },
+  components: {}
+}
+</script>
+
+<style lang='stylus' scoped>
+</style>
